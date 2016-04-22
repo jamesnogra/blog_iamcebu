9e22c57702e3fd1037c348340eb270422f24dd17
James Arnold Nogra <james.nogra@ripeconcepts.com>
2016-04-22 15:20:02 +0800

    Font styling.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+
+    <head>
+        <title>I Am Cebu - Blog</title>
+        <link href='https://fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="{{url('/css/main.css')}}">
+    </head>
+
+
+    <body>
+        <h1>I am Cebu</h1>
+        @yield('content')
+    </body>
+
+
+</html>
